3090768725fc4344833ace089ca2532c0cf19cca
Anupam Samanta <anupam96786@gmail.com>
2022-04-04 21:21:53 +0530

    led animation improved

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from time import sleep
 from pyfirmata import Arduino
 from config import PORT
 import sounddevice as sd
@@ -6,20 +5,23 @@ import numpy as np
 import keyboard
 
 board = Arduino(PORT)
+prev_val = 1
 
 def print_sound(indata, outdata, frames, time, status):
+    global prev_val
     volume_norm = int((np.linalg.norm(indata) * 10).round())
     if volume_norm < 100:
         val = volume_norm // 10
         if val > 0:
-            pin = val + 1
-            for i in range(2, pin + 1):
-                board.digital[i].write(1)
-                sleep(0.03)
-            for i in range(2, pin + 1):
-                board.digital[i].write(0)
-    else:
-        print('overflow')
+            if prev_val < val:
+                for i in range(prev_val, val + 1):
+                    pin = i + 1
+                    board.digital[pin].write(1)
+            elif prev_val > val:
+                for i in range(prev_val + 1, val, -1):
+                    pin = i + 1
+                    board.digital[pin].write(0)
+            prev_val = val
 
 with sd.Stream(callback=print_sound):
     keyboard.wait('q')
